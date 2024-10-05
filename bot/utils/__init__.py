a771a7edf5ccc476614e9e8636fe6d857c294e8e
Foad Davoodi <myf.davoodi@gmail.com>
2024-10-05 22:19:35 +0330

    Delete bot directory

--- bot/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .logger import logger, info, warning, debug, success, error, critical
-from . import launcher
-
-
-import os
-
-if not os.path.exists(path="sessions"):
-    os.mkdir(path="sessions")
